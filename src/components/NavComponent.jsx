9869aadae3d509d6d057d76cab21ff6903fb88ee
Joan <joanalemany26@gmail.com>
2024-05-22 17:37:21 +0200

    navbar added

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,7 +1,14 @@
-export const NavComponent = () => {
-return(
-  <div className="nav">
+import { Link } from 'react-router-dom';
 
-  </div>
+export const NavComponent = () => {
+
+return(
+  <nav className="nav">
+    <article className="nav__logo">OxygenPhotos</article>
+    <ul className='nav__list'>
+      <li className='nav__list__option'><Link to="/">Inicio</Link></li>
+      <li className='nav__list__option'><Link to="fav">Favoritos</Link></li>
+    </ul>
+  </nav>
 )
 };
