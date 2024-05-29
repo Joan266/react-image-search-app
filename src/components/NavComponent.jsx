d52e7cc2580930f5447d6470ae29679f9d4391b1
Joan <joanalemany26@gmail.com>
2024-05-29 12:04:40 +0200

    made navbar interactive

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,16 +1,39 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { SearchBarComponent } from './SearchBarComponent';
 
 export const NavComponent = () => {
-return(
-  <nav className="nav">
-    <article className="nav__logo"><Link to="/">OxygenPhotos</Link></article>
-    <div className="nav__search-bar-container">
-      <SearchBarComponent/>
-    </div>
-    <div className='nav__fav-link' >
-      <Link to="/fav">Favoritos</Link> 
-    </div>
-  </nav>
-)
+  const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${(isScrolled && isHomePage) ? 'nav--regular' : 'nav--top-home'}`}>
+      <article className="nav__logo">
+        <Link to="/">OxygenPhotos</Link>
+      </article>
+      <div className="nav__search-bar-container">
+        <SearchBarComponent />
+      </div>
+      <div className="nav__fav-link">
+        <Link to="/fav">Favoritos</Link>
+      </div>
+    </nav>
+  );
 };
