5d169cefbdffbe8ef993c7001f850a5d3ea0150c
Joan <118159246+Joan266@users.noreply.github.com>
2024-06-10 13:50:31 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { SearchSliceReducer } from './slices/SearchSlice/SearchSlice.js'
 import { RandomSliceReducer } from './slices/RandomSlice/RandomSlice.js'
-import { FavSliceReducer } from './slices/favSlice.js'
+import { FavSliceReducer } from './slices/FavSlice.js'
 export const store = configureStore({
   reducer: {
     search: SearchSliceReducer,
